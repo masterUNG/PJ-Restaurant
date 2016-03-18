ede02c30c0c58a0dea082ec6d21c6f08c61e6b69
masterUNG <phrombutr@gmail.com>
2016-03-18 11:13:51 +0700

    Test Add and Delete All Finish

--- app/src/main/java/appewtc/masterung/pjrestaurant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/pjrestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.pjrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,6 +17,26 @@ public class MainActivity extends AppCompatActivity {
         //Request SQLite
         myManage = new MyManage(this);
 
+        //Test Add Value
+        //testAddValue();
+
+        //Delete SQLite
+        deleteSQLite();
+
     }   // Main Method
+
+    private void deleteSQLite() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+        sqLiteDatabase.delete(MyManage.food_table, null, null);
+        sqLiteDatabase.delete(MyManage.order_table, null, null);
+    }
+
+    private void testAddValue() {
+        myManage.addUser("user", "pass", "name", "address", "phone");
+        myManage.addFood("food", "descrip", "price", "icon", "image");
+        myManage.addOrder("date", "name", "address", "phone", "food", "amount", "total");
+    }
 
 }   // Main Class
